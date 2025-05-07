a9ace5a403b16989d694e5a55b43e453cb1f105b
Arshad Afaque <arshad@infomericainc.in>
2025-05-07 15:49:16 +0530

    Spring Test

---
+++ src/test/java/com/dynamodb/curd/SpringbootCurdDynamodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dynamodb.curd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCurdDynamodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
