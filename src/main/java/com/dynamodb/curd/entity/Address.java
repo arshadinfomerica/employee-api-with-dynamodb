3ac60a0213d2a73974a42cb5a6f6161d2f44d762
Arshad Afaque <arshad@infomericainc.in>
2025-05-07 15:47:47 +0530

    DynamoDB Table Create

---
+++ src/main/java/com/dynamodb/curd/entity/Address.java
@@ -0,0 +1,22 @@
+package com.dynamodb.curd.entity;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBDocument;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@DynamoDBDocument
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+	
+	@DynamoDBAttribute
+	private String cityName;
+	@DynamoDBAttribute
+	private String countryName;
+
+}
