802aa819ec3373761aa19bcf4589665d4f0f2f0f
Arshad Afaque <arshad@infomericainc.in>
2025-05-14 16:03:10 +0530

    clean some code

--- src/main/java/com/dynamodb/curd/repository/EmployeeRepository.java
+++ src/main/java/com/dynamodb/curd/repository/EmployeeRepository.java
@@ -1,7 +1,6 @@
 package com.dynamodb.curd.repository;
 
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
